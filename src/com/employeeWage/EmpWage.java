cafa3caf1159db6b25302aee1657a3df5e068875
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 22:37:53 +0530

    Added max hrs using while

--- src/com/employeeWage/EmpWage.java
+++ src/com/employeeWage/EmpWage.java
@@ -5,31 +5,36 @@ public class EmpWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS = 100;
 
 	public static void main(String[] args) {
 
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;
 		int totalEmpWage = 0;
-		for (int days = 0; days < NUM_OF_WORKING_DAYS; days++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalWorkingDays <= NUM_OF_WORKING_DAYS && totalEmpHrs <= MAX_HRS) {
+			int empHrs = 0;
+			int empWage = 0;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_FULL_TIME:
 				empHrs = 8;
+				totalWorkingDays++;
 				break;
 			case IS_PART_TIME:
 				empHrs = 4;
+				totalWorkingDays++;
 				break;
 			default:
 				empHrs = 0;
 
 			}
 			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpHrs += empHrs;
 			totalEmpWage += empWage;
 			System.out.println("Emp Wage : " + empWage);
 		}
-		System.out.println("Total Employee Wage : " + totalEmpWage);
+		System.out.println("Total Emp Wage : " + totalEmpWage);
 
 	}
 }
