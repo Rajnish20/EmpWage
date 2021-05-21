b9eb3c62a1d4ec1b21c1634aae39d9c7722cc631
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-21 09:57:00 +0530

    Refactored to object oriented

--- src/com/employeeWage/EmpWage.java
+++ src/com/employeeWage/EmpWage.java
@@ -1,40 +1,51 @@
 package com.employeeWage;
 
 public class EmpWage {
-	public static final int IS_FULL_TIME = 2;
-	public static final int IS_PART_TIME = 1;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS = 100;
 
 	public static void main(String[] args) {
 
-		int totalEmpWage = 0;
+		int numberOfWorkingdays = 20;
+		int maxHrs = 100;
+		int empRatePerHour = 20;
+		
+		calculateWage calculatewage = new calculateWage();
+		int totalHrs = calculatewage.totalHrs(numberOfWorkingdays,maxHrs);
+		int totalWage = calculatewage.totalWage(totalHrs,empRatePerHour);
+		System.out.println(totalWage);
+	}
+}
+
+class calculateWage {
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+	
+
+	public int totalHrs(int numberOfWorkingDays, int maxHrs) {
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		while (totalWorkingDays <= NUM_OF_WORKING_DAYS && totalEmpHrs <= MAX_HRS) {
+		while (totalWorkingDays <= numberOfWorkingDays && totalEmpHrs <= maxHrs) {
 			int empHrs = 0;
-			int empWage = 0;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_FULL_TIME:
 				empHrs = 8;
-				totalWorkingDays++;
 				break;
 			case IS_PART_TIME:
 				empHrs = 4;
-				totalWorkingDays++;
 				break;
 			default:
 				empHrs = 0;
 
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Employee Hours For Day : " + totalWorkingDays + " " + empHrs);
 			totalEmpHrs += empHrs;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage : " + empWage);
+			totalWorkingDays++;
 		}
-		System.out.println("Total Emp Wage : " + totalEmpWage);
+		return totalEmpHrs;
+			
+	}
 
+	public int totalWage(int totalHrs, int empRatePerHour) {
+		return empRatePerHour * totalHrs;
 	}
 }
